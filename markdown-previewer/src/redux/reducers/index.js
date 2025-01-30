b55b98db975153bb1d24a1e0aebdd36a13b4be07
Manuel Barone <baronemanuel05@gmail.com>
2025-01-30 21:16:29 +0100

    update index.js

--- markdown-previewer/src/redux/reducers/index.js
+++ markdown-previewer/src/redux/reducers/index.js
@@ -2,18 +2,18 @@ import { CHANGE } from "../actions";
 
 const initialState = {
   editorText:
-    "# MARKDOWN DI ESEMPIO H1  \n" +
-    "## Titolo H2  \n" +
-    "[Link](https://github.com/Zoltan3do)  \n" +
-    "`const ciao = 'Hello World!'`  \n" +
+    "# MARKDOWN DI ESEMPIO H1  \n\n" +
+    "## Titolo H2  \n\n" +
+    "[Link](https://github.com/Zoltan3do)  \n\n" +
+    "`const ciao = 'Hello World!'`  \n\n" +
     "```javascript  \n" +
     "console.log('Hello World 2.0')  \n" +
     "console.log('ZOltan3do')  \n" +
-    "```  \n" +
+    "```  \n\n" +
     "- ciao  \n" +
-    "* come ti chiami?  \n" +
-    "> citazione  \n" +
-    "![ciao](https://www.adesso-online.de/sites/adesso.spotlight-verlag.de/files/styles/mode_content/public/2018-10/ciao_compleanno.jpg?itok=2oc5NRPs)  \n" +
+    "* come ti chiami?  \n\n" +
+    "> citazione  \n\n" +
+    "![ciao](https://www.adesso-online.de/sites/adesso.spotlight-verlag.de/files/styles/mode_content/public/2018-10/ciao_compleanno.jpg?itok=2oc5NRPs)  \n\n" +
     "**Testo** in __grassetto__!",
 };
 
