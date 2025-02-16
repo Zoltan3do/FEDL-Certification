140c0d29247aa28a0e14bd141a7e2189118b257d
Manuel Barone <baronemanuel05@gmail.com>
2025-02-16 20:44:50 +0100

    update lengthes

--- 25-5-clock/src/components/Lengthes.jsx
+++ 25-5-clock/src/components/Lengthes.jsx
@@ -4,9 +4,9 @@ function Lengthes({ type, time }) {
       <div className="d-flex flex-column ">
         <h2 className="text-light mb-3 fw-normal">{type} Length</h2>
         <div className="d-flex justify-content-center text-light">
-          <i class="fa-solid fa-arrow-down cursor-pointer"></i>
+          <i class="fa-solid fa-arrow-down cursor-pointer fs-3"></i>
           <p>{time}</p>
-          <i class="fa-solid fa-arrow-up cursor-pointer"></i>
+          <i class="fa-solid fa-arrow-up cursor-pointer fs-3"></i>
         </div>
       </div>
     </>
